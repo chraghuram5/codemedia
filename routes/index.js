36789344ea4e32d45989d2e692e12751d34ef6c8
chraghuram5 <ch.raghuram5@gmail.com>
2020-06-03 00:12:54 +0530

    added users controller

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const homeController=require('../controllers/home_controller');
 console.log("router loaded");
 
 router.get('/home',homeController.home);
+router.use('/users',require('./users'));
+
 
 module.exports=router;
