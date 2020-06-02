8546ec44de7bc5be5802107a73d5bca84a0b733e
chraghuram5 <ch.raghuram5@gmail.com>
2020-06-02 23:40:38 +0530

    added routes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port=8000;
 
 const app=express();
 
+//tells app to use express router
+//use express router
+app.use('/',require('./routes/index'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`error starting server on port: ${port}`);
