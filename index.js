bce1d196fe74ac16de840a3f5e975945c2536829
chraghuram5 <ch.raghuram5@gmail.com>
2020-06-04 19:49:06 +0530

    add authentication

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
 const cookieParser=require('cookie-parser');
 
+//used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('passport-local');
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('./assets'));
 
-//use it before routes
-app.use(expressLayouts);
 
 //extract styles and scripts from subpages into the layout
 app.set('layout extractStyles',true);
@@ -22,6 +25,23 @@ app.use('/',require('./routes/index'));
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name: 'codemedia',
+    //change the secret before deployment in production mode
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        maxAge: (1000* 60* 100)
+    }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//use it before routes
+app.use(expressLayouts);
 
 app.listen(port, function(err){
     if(err){
