d4d9b293beffa922a6aa95f1d4bb24ee4da3a945
chraghuram5 <ch.raghuram5@gmail.com>
2020-06-08 00:50:02 +0530

    add SASS

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -60,3 +60,9 @@ module.exports.create = function (req, res) {
 module.exports.createSession = function (req, res) {
    return res.redirect('/home');
 }
+
+//
+module.exports.destroySession=function(req,res){
+    req.logout();
+    return res.redirect('/home');
+}
